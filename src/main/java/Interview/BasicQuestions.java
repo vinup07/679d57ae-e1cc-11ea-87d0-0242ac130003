e327a5ce2819d22ca85b12f4098f202984a53bc2
vinup07 <vinayak.gu@gmail.com>
2020-08-24 14:57:28 +1000

    Update BasicQuestions.java

--- src/main/java/Interview/BasicQuestions.java
+++ src/main/java/Interview/BasicQuestions.java
@@ -3,26 +3,18 @@ package Interview;
 import Interview.utils.getNames;
 
 public class BasicQuestions {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String[] nameArray = new String[]{
-                "Sarah", "Anna", "Bentley", "ViV", "AddA", "Ben", "jack","Jonathon"
+                "Sarah", "Anna", "Bentley", "ViV", "AddA", "Ben", "jack", "Jonathon"
         };
-
-        //The method "checkPallindrome" below is a third party library
-        // "checkPallindrome" method checks if the string is a Pallindrome.
-        // E.g. "Anna" will return true        
-        public static boolean checkPallindrome(String name){
-            //Some Code
-            return true;
-        }
-        //Question: Create a list of  all the strings that are pallindrome's in the "namearray" using the checkPallindrome method
-
     }
-
-
-
-
-
-
+    
+    //Question: Create a list of  all the strings that are pallindrome's in the "namearray" using the checkPallindrome method
+    //The method "checkPallindrome" below is a third party library
+    // "checkPallindrome" method checks if the string is a Pallindrome.
+    // E.g. "Anna" will return true
+    public static boolean checkPallindrome(String name){
+        //Some Code
+        return true;
+    }
 }
